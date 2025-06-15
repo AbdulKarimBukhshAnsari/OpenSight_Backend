c381f2d314cc0e9c6bdd1f0a2c688643ffcee0f4
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2025-06-15 23:53:35 +0500

    feat : CORS and Configuration setting has been updated

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from "express"
+import cookieParser from "cookie-parser";
+import cors from "cors"
+
+
+const app = express();
+const CORS_ORIGIN = process.env.CORS_ORIGIN ;
+
+// defining the configurations
+
+// for defining the ORGIN URL by which the request could be made
+app.use(cors({
+    origin : CORS_ORIGIN ,
+    credentials : true 
+}))
+
+app.use(express.json({limit : '16kb'}));
+
+app.use(express.urlencoded({extended : true , limit : '16kb'}));
+
+app.use(express.static('public'));
+
+app.use(cookieParser());
+
+
+export default app ; 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import 'dotenv/config'
 import DB_Connection from './db/index.js'
+import app from './app.js';
 
-DB_Connection();
+const PORT = process.env.PORT || 8000 ; 
+
+
+DB_Connection()
+.then(()=>{
+    app.listen(PORT , () => {
+        console.log(`Application is live on http://localhost:${PORT}`)
+    })
+})
+.catch((error) => {
+    console.log('Mongo DB connextion Failed!!!' , error);
+})
