c4efe49920aa8639d124533aec3d0ad1418baf91
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2025-06-18 11:14:20 +0500

    feat(model) : Video Model has been defined

---
+++ src/models/video.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const videoSchema = new mongoose.Schema( {
+    videoFile : {
+        type : String ,
+    },
+    thumbnail : {
+        type : String 
+    },
+    owner : {
+        type : mongoose.Schema.Types.ObjectId ,
+        ref : "User"
+    },
+    title : {
+        type : String ,
+        trim : true , 
+        required : [true , "Title is Required"]
+    },
+    description : {
+        type : String , 
+        trim : true , 
+        required : [true , "Title is Required"]
+    },
+    duration : {
+        type : Number ,
+    },
+    views : {
+        type : Number ,
+    },
+    isPublished : {
+        type : Boolean ,
+    }
+} , {timestamps : true})
+
+
+export const Video = mongoose.model('Video' , videoSchema) ;
