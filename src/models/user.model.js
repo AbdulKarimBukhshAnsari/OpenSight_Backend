dd180d6111f739e81084f2fd8388d46c6e398609
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2025-07-02 23:01:23 +0500

    feat : Defined the User model , video model , plus password hashing and jwt token generation has been completed

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema({
     userName : {
@@ -45,5 +47,47 @@ const userSchema = new mongoose.Schema({
         type : String 
     }
 } , {timestamps : true}) ;
+
+userSchema.pre('save' , 
+    async function(next) {
+        if(!this.isModified('password')) return next();
+        this.password = await bcrypt.hash(this.password , 10)
+        return next();
+    }
+)
+
+// injecting few metods with schema
+
+userSchema.methods.isPasswordCorrect = async function  (password) {
+    return await  bcrypt.compare(password , this.password);
+}
+
+userSchema.methods.generateAccessToken =  function(){
+    return jwt.sign(
+        {
+            _id : this._id , // we will get this from the MongoDB
+        },
+        process.env.ACESS_TOKEN_SECRET,
+        {
+            expiresIn : process.env.ACESS_TOKEN_EXPIRY
+        }
+    )
+
+}
+
+
+userSchema.methods.generateRefreshToken =  function(){
+    return jwt.sign(
+        {
+            _id : this._id , // we will get this from the MongoDB
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+
+}
+
 
 export const User = mongoose.model('User' , userSchema) ;
