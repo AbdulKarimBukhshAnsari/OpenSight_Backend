2e6c5159620ba48960c25f17bad29222cad56ff8
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2025-07-19 23:48:50 +0500

    refactor : doing validation of the data coming from teh frontend

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,13 @@
 import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js";
 
 const registerUser = asyncHandler(async (req , res , next) => {
-    res.status(200).json({
-        message : "karim"
-    })
+    // get the data coming from the frontend and do validation 
+    const {email , password ,  fullName , userName } = req.body ;
+    if([email, password , fullName  , userName].some((element)=> element?.trim() === "") || 
+    [email , password , fullName , userName].some((element) => !element)) {
+       throw new ApiError(400 , "All Fields are Required");
+    }
 })
 
 export {registerUser}
